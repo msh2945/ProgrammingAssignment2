38e46616353c2c17d902b3b06219d9535f3f40c1
Mehdi <65007069+msh2945@users.noreply.github.com>
2020-06-24 16:07:14 +0900

    Add files via upload

---
+++ Assignment_2, Mehdi.R
@@ -0,0 +1,42 @@
+
+#################### Making a Matrics that can cash its invers ##########
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+
+##########################Solution for the inverse of Matrics ########################
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
+
+############# Example ########################
+
+my_matrix <- makeCacheMatrix(matrix(10:13, 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+
+cacheSolve(my_matrix)
